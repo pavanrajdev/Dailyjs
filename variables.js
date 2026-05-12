11f84551e0400f7a7f0e858af32ed4c141a3cfd3
pavanrajdev <pavanrajmatangi921@gmail.com>
2026-05-12 22:31:20 +0530

    day-1.1

--- variables.js
+++ variables.js
@@ -1,4 +1,10 @@
 
-var a=10;
-let b=20;
-const c=30;
+var a=10;//it is a global or function scope variable 
+        // the variable is hoisted by stored as undefined
+        //we can reinitialize and update
+let b=20;//it is a block scope variable
+        //it is also hoisted but NOT initialized with undefined Accessing before declaration gives error (Temporal Dead Zone)
+        //we can update but cant reinitialize
+const c=30;//it is constant
+            //hoisted
+            // you can niether reinitialize nor update the value 
