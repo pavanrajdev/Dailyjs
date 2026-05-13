c6e875a16ed654c414c4645e2e1ab9f631e745f0
pavanrajdev <pavanrajmatangi921@gmail.com>
2026-05-13 17:46:12 +0530

    Day-2.4

---
+++ closures.js
@@ -0,0 +1,19 @@
+//closures are the functions defined inside another function 
+//the inner function can access the variables of outer functions and scope of outer function
+//helps achieve private variables and state maintainance(react);
+
+function outerCounter(){
+  let count=0;
+  function innerCounter(){
+    console.log(++count);
+  }
+  function getCount(){
+    return count;
+  }
+  return {innerCounter,getCount};
+}
+
+const counter=outerCounter();
+counter.innerCounter();
+counter.innerCounter();
+console.log(counter.getCount());
